707335d5ca060e2829b14c949e6b6d59fffa2f60
Arydiane <arydianejardim@gmail.com>
2023-09-22 20:45:05 -0300

    adiciona componente welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Quiz</h1>
+      <h1>Quiz de programação</h1>
+      <Welcome />
     </div>
   )
 }
