5d905c577785a7e116cf977088e193b5a2297124
Arydiane <arydianejardim@gmail.com>
2023-09-22 20:43:09 -0300

    cria componente welcome

---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,15 @@
+import styles from "./Welcome.module.scss";
+import Quiz from "../../assets/img/quiz.svg";
+
+const Welcome = () => {
+  return (
+    <section id={styles.welcome}>
+      <h2>Seja bem-vindo!</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button>Iniciar</button>
+      <img src={Quiz} alt="Início do quiz" />
+    </section>
+  );
+};
+
+export default Welcome;
