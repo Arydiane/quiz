2a72bf79cd4524feffaf952b20b7986a259798ac
Arydiane <arydianejardim@gmail.com>
2023-09-22 21:30:57 -0300

    adiciona funcionalidade ao botão iniciar dispara uma mudança de estado no jogo

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,12 +1,16 @@
 import "./Welcome.css";
 import Quiz from "../../assets/img/quiz.svg";
+import { useQuiz } from "../../context/quiz";
 
 const Welcome = () => {
+
+  const [quizState, dispatch] = useQuiz()
+
   return (
     <section id="welcome">
       <h2>Seja bem-vindo!</h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
       <img src={Quiz} alt="Início do quiz" />
     </section>
   );
