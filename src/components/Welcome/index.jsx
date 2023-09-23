cb1d4979093f363caddfc5a082e89892f748147b
Arydiane <arydianejardim@gmail.com>
2023-09-22 21:05:53 -0300

    adiciona estilização em welcome

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,9 +1,9 @@
-import styles from "./Welcome.css";
+import "./Welcome.css";
 import Quiz from "../../assets/img/quiz.svg";
 
 const Welcome = () => {
   return (
-    <section id={styles.welcome}>
+    <section id="welcome">
       <h2>Seja bem-vindo!</h2>
       <p>Clique no botão abaixo para começar:</p>
       <button>Iniciar</button>
