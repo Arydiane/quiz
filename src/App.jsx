4cd03ee8335ad30e5eb31906ae6cf1672c1b4f4f
Arydiane <arydianejardim@gmail.com>
2023-09-22 22:01:54 -0300

    atualiza componente app adiciona a ação de embarralhar questões ao iniciar exibe componente baseado no estágio do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import Welcome from './components/Welcome'
+import { useEffect } from "react";
+import Welcome from "./components/Welcome";
+import { useQuiz } from "./context/quiz";
+import Question from "./components/Question";
+import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useQuiz();
+
+  useEffect(() => {
+    //embaralhar as perguntas
+    dispatch({ type: "REORDER_QUESTIONS" });
+  }, []);
+
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Quiz de programação</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
