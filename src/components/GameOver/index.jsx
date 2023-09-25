2da54c201c57a9c4f45fe443439d2463288dbf4c
Arydiane <arydianejardim@gmail.com>
2023-09-25 17:18:19 -0300

    cria componente game over

---
+++ src/components/GameOver/index.jsx
@@ -0,0 +1,22 @@
+import "./Gameover.css";
+import { useQuiz } from "../../context/quiz";
+import WellDone from "../../assets/img/welldone.svg";
+
+function GameOver() {
+  const [quizState, dispach] = useQuiz();
+
+  return (
+    <section id="gameover">
+      <h2>Fim de Jogo!</h2>
+      <p>Pontuação: {quizState.score}</p>
+      <p>
+        Você acertou {quizState.score} de {quizState.questions.length}{" "}
+        perguntas.
+      </p>
+      <img src={WellDone} alt="Fim do Quiz" />
+      <button onClick={() => dispach({ type: "NEW_GAME" })}>Reiniciar</button>
+    </section>
+  );
+}
+
+export default GameOver;
