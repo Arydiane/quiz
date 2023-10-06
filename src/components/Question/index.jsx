e5377836108351ac8c01e34ad6ea725f4698639c
Arydiane <arydianejardim@gmail.com>
2023-10-06 20:47:10 -0300

    atualiza compoente question adiciona botão de dica e excluir uma opção

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -5,14 +5,14 @@ import Option from "../Option";
 
 function Question() {
   const [quizState, dispatch] = useQuiz();
-
   const currentQuestion = quizState.questions[quizState.currentQuestion];
 
   const onSelectOption = (option) => {
-    dispatch({ type: "CHECK_ANSWER", 
-    payload: {answer: currentQuestion.answer, option }
-  })
-  }
+    dispatch({
+      type: "CHECK_ANSWER",
+      payload: { answer: currentQuestion.answer, option },
+    });
+  };
 
   return (
     <section id="question">
@@ -22,9 +22,28 @@ function Question() {
       <h2>{currentQuestion.question}</h2>
       <div id="options-container">
         {currentQuestion.options.map((option) => (
-          <Option key={option} option={option} answer={currentQuestion.answer} selectOption={() => onSelectOption(option)} />
+          <Option
+            key={option}
+            option={option}
+            answer={currentQuestion.answer}
+            selectOption={() => onSelectOption(option)}
+            hide={quizState.optionToHide === option ? "hide" : null}
+          />
         ))}
       </div>
+      {!quizState.answerSelected && !quizState.help && (
+        <>
+          {currentQuestion.tip && (
+            <button onClick={() => dispatch({ type: "SHOW_TIP" })}>Dica</button>
+          )}
+          <button onClick={() => dispatch({ type: "REMOVE_OPTION" })}>
+            Excluir uma opção
+          </button>
+        </>
+      )}
+      {!quizState.answerSelected && quizState.help === "tip" && (
+        <p>{currentQuestion.tip}</p>
+      )}
       {quizState.answerSelected && (
         <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
           Continuar
