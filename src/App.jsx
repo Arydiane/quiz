160f86e8963afa237b00a5f1d4f3155ce2b1c6cc
Arydiane <arydianejardim@gmail.com>
2023-10-06 12:49:31 -0300

    atualiza app adiciona a escolha de categoria

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,17 @@ import { useQuiz } from "./context/quiz";
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
 import GameOver from "./components/GameOver";
+import PickCategory from "./components/PickCategory";
 import "./App.css";
 
 function App() {
   const [quizState, dispatch] = useQuiz();
 
-  useEffect(() => {
-    //embaralhar as perguntas
-    dispatch({ type: "REORDER_QUESTIONS" });
-  }, []);
-
   return (
     <div className="App">
       <h1>Quiz de programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
       {quizState.gameStage === "Playing" && <Question />}
       {quizState.gameStage === "End" && <GameOver />}
     </div>
